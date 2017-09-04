0e5b7b5075f4e607ee04ed7ab923622de6a2bf13
root <root@server.example.com>
2017-09-03 20:57:18 -0700

    adding apache module

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,11 @@
 node 'node' {
-	include sudo,ssh,postfix,mysql
+	include sudo,ssh,postfix,mysql,apache
+	
+	apache::vhost { 'www.example.com':
+		port => 80,
+		docroot => '/var/www/www.example.com',
+		ssl => false,
+		priority => 10,
+		serveraliases => 'home.example.com',
+	}
 }
